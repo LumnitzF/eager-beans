08b01e15749fc7962ecc63a0fb9d65629e474e6a
Fritz Lumnitz <fritz@lumoos.de>
2019-11-09 23:54:46 +0100

    Added javadoc

--- src/main/java/io/github/lumnitzf/eagerbeans/Eager.java
+++ src/main/java/io/github/lumnitzf/eagerbeans/Eager.java
@@ -1,11 +1,31 @@
 package io.github.lumnitzf.eagerbeans;
 
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Stereotype;
+import javax.enterprise.inject.spi.DefinitionException;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a bean to be initialized as soon as its associated context gets activated.
+ * <p>
+ * This annotation may be put on a class or on a {@link Stereotype}.
+ * </p>
+ * <p>
+ * The annotated bean must <b>not</b> have scope {@link Dependent @Dependent}. In this case a
+ * {@link DefinitionException} will be thrown at deployment time.
+ * </p>
+ *
+ * @implNote The bean initialization will be triggered by calling {@link Object#toString() toString()} on the bean
+ * reference. In case the toString() computation is considered too expensive, annotated classes may implement {@link
+ * EagerInitializable}. Initialization then will be triggered by calling the no-op {@link EagerInitializable#init()}.
+ */
+@SuppressWarnings("deprecation")
+@Inherited
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
